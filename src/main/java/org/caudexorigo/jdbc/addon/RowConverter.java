a39d1d4774cb94262d3b8ace33099d8f69ac21b8
Bruno René Santos <bruno-r-santos@telecom.pt>
2014-03-18 14:31:14 +0000

    new RowConverter & maven project

---
+++ src/main/java/org/caudexorigo/jdbc/addon/RowConverter.java
@@ -0,0 +1,32 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package org.caudexorigo.jdbc.addon;
+
+import java.sql.ResultSet;
+
+/**
+ *
+ * @author brsantos
+ * @param <T> row type
+ */
+public abstract class RowConverter<T> implements org.caudexorigo.jdbc.RowConverter<T>
+{
+
+	public T process(ResultSet rs)
+	{
+		try
+		{
+			return transform(rs);
+		}
+		catch (Throwable e)
+		{
+			throw new RuntimeException(e);
+		}
+	}
+
+	protected abstract T transform(ResultSet rs) throws Throwable;
+
+}
