137c177526fca45bccec46fb36ae6ce99cf4037c
Bruno René Santos <bruno-r-santos@telecom.pt>
2014-05-15 15:31:19 +0100

    MapConverter with Generics

--- src/main/java/org/caudexorigo/jdbc/addon/MapConverter.java
+++ src/main/java/org/caudexorigo/jdbc/addon/MapConverter.java
@@ -16,19 +16,25 @@ import org.slf4j.LoggerFactory;
  *
  * @author brsantos
  */
-public class MapConverter extends RowConverter<Map<String, Object>>
+public class MapConverter<T> extends RowConverter<Map<String, T>>
 {
 
 	private final static Logger log = LoggerFactory.getLogger(MapConverter.class);
+	private Class<T> colClass;
+
+	public MapConverter(Class<T> colClass)
+	{
+		this.colClass = colClass;
+	}
 
 	@Override
-	protected Map<String, Object> transform(ResultSet rs) throws Throwable
+	protected Map<String, T> transform(ResultSet rs) throws Throwable
 	{
 		ResultSetMetaData metadata = rs.getMetaData();
-		Map<String, Object> map = new HashMap<>();
+		Map<String, T> map = new HashMap<>();
 		for (int i = 1; i <= metadata.getColumnCount(); i++)
 		{
-			map.put(metadata.getColumnName(i), rs.getObject(i));
+			map.put(metadata.getColumnName(i), rs.getObject(i, colClass));
 		}
 		return map;
 	}
